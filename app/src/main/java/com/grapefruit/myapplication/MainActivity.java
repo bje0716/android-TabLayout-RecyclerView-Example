f6edb4af644eb76c30a1c2bbf22cb6a63b39cf25
bje0716 <byon0716@gmail.com>
2016-04-20 11:04:57 +0900

    not nullable

--- app/src/main/java/com/grapefruit/myapplication/MainActivity.java
+++ app/src/main/java/com/grapefruit/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.grapefruit.myapplication;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -86,7 +85,6 @@ public class MainActivity extends AppCompatActivity {
             mPage = getArguments().getInt(ARG_PAGE);
         }
 
-        @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.viewpager_item, container, false);
